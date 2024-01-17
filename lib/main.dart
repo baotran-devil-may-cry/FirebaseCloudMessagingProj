283158eeac2c4c999bbe8708e6e39f9c78a10dc4
Bảo Trần <tranquocbaohuflit@gmail.com>
2024-01-17 18:53:50 +0700

    chore: added some comment

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Get local or from Github Secrets
     final title = Env.androidAppName;
 
     return MaterialApp(
